c14689f1da95f67282f7cb1a7b5a470d361694a4
Quentin Caritey <quentin.caritey@neoxia.com>
2022-01-19 17:45:21 +0100

    wip

--- game.py
+++ game.py
@@ -3,12 +3,23 @@ import arcade
 import pickle
 import os
 
-SPRITE_SIZE = 200
-COLUMNS = 3
-HEIGHT = 4
+SPRITE_SIZE = 80
+
+MAP = """
+    ##.##
+    ##o##
+    ##o##
+    ##To#
+    ###T#
+    ###o#
+    T#T#T
+    ##o#T
+    #TT##
+    ####o
+"""
 
 VOID = '#'
-PLAYER = '.'
+START = '.'
 COIN = 'o'
 TREE =  'T'
 
@@ -23,14 +34,17 @@ REWARD_COIN = 50
 REWARD_NOTHING = 10
 
 class Environment:
-    def __init__(self):
+    def __init__(self, text):
 
         self.__states = {}
-        for row in range(HEIGHT):
-            for col in range(COLUMNS):
-                self.__states[(row, col)] = VOID
-        self.__states[(0, 1)] = PLAYER
-        self.__start = (0, 1)
+        lines = list(map(lambda x: x.strip(), text.strip().split('\n')))
+        for row in range(len(lines)):
+            for col in range(len(lines[row])):
+                self.__states[(row, col)] = lines[row][col]
+                if(lines[row][col] == START):
+                    self.__start = (row, col)
+        self.width = len(lines[0])
+        self.height = len(lines)
 
     @property
     def start(self):
@@ -46,11 +60,11 @@ class Environment:
     def apply(self, agent, action):
         state = agent.state
         if action == NOTHING:
-            new_state = (state[0], state[1])
+            new_state = (state[0] + 1, state[1])
         elif action == LEFT:
-            new_state = (state[0], state[1] - 1)
+            new_state = (state[0] + 1, state[1] - 1)
         elif action == RIGHT:
-            new_state = (state[0], state[1] + 1)
+            new_state = (state[0] + 1, state[1] + 1)
 
         if new_state in self.__states:
             if self.__states[new_state] == TREE:
@@ -130,7 +144,11 @@ class Agent:
 
 class Game(arcade.Window):
     def __init__(self, agent):
-        super().__init__(COLUMNS * SPRITE_SIZE, HEIGHT * SPRITE_SIZE, 'Subway Surfer')
+        super().__init__(
+            agent.environment.width * SPRITE_SIZE,
+            agent.environment.height * SPRITE_SIZE,
+            'Subway Surfer'
+        )
         self.__environment = agent.environment
         self.__agent = agent
         self.__iteration = 1
@@ -139,18 +157,22 @@ class Game(arcade.Window):
 
         self.background = arcade.load_texture("./images/grass.jpg")
         self.player = arcade.Sprite("./images/car.png", 1)
-        # self.coins = arcade.SpriteList()
-        self.trees = arcade.SpriteList()
-        # for state in self.__environment.states:
-        #     if self.__environment.getContent(state) == WALL:
-        #         sprite = arcade.Sprite(":/tiles/stone.png", 0.5)
-        #         sprite.center_x = (state[1] + 0.5) * SPRITE_SIZE
-        #         sprite.center_y = self.height - (state[0] + 0.5) * SPRITE_SIZE
-        #         self.walls.append(sprite)
-        # self.goal = arcade.Sprite(":resources:images/items/flagRed1.png", 0.5)
-        # self.goal.center_x = (self.__environment.goal[1] + 0.5) * SPRITE_SIZE
-        # self.goal.center_y = self.height - (self.__environment.goal[0] + 0.5) * SPRITE_SIZE
 
+        self.trees = arcade.SpriteList()
+        for state in self.__environment.states:
+            if self.__environment.getContent(state) == TREE:
+                sprite = arcade.Sprite("./images/tree.png", 0.5)
+                sprite.center_x = (state[1] + 0.5) * SPRITE_SIZE
+                sprite.center_y = self.height - (state[0] + 0.5) * SPRITE_SIZE
+                self.trees.append(sprite)
+
+        self.coins = arcade.SpriteList()
+        for state in self.__environment.states:
+            if self.__environment.getContent(state) == COIN:
+                sprite = arcade.Sprite("./images/coin.png", 0.5)
+                sprite.center_x = (state[1] + 0.5) * SPRITE_SIZE
+                sprite.center_y = self.height - (state[0] + 0.5) * SPRITE_SIZE
+                self.coins.append(sprite)
         self.update_agent()
 
     def get_center(self, coordinates):
@@ -167,15 +189,16 @@ class Game(arcade.Window):
     def on_draw(self):
         arcade.start_render()
         arcade.draw_texture_rectangle(self.width / 2, self.height / 2, self.width, self.height, self.background)
-        # self.walls.draw()
-        # self.goal.draw()
+        self.trees.draw()
+        self.coins.draw()
         self.player.draw()
         arcade.draw_text(f"#{self.__iteration} Score : {self.__agent.score}", 10, 10, arcade.csscolor.WHITE, 20)
 
     def on_update(self, delta_time):
-        action = self.__agent.best_action()
-        reward = self.__agent.do(action)
-        self.update_agent()
+        if self.__agent.state[0] < self.__agent.environment.height:
+            action = self.__agent.best_action()
+            reward = self.__agent.do(action)
+            self.update_agent()
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.R:
@@ -185,7 +208,7 @@ class Game(arcade.Window):
 if __name__ == "__main__":
     agent_filename = 'agent.dat'
 
-    env = Environment()
+    env = Environment(MAP)
     agent = Agent(env)
     if os.path.exists(agent_filename):
         agent.load(agent_filename)
