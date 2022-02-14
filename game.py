808b742fef0b0d3ac5f8db9551fb7a796e2a808a
belma <belmajkarim29@gmail.com>
2022-02-14 18:24:40 +0100

    réseau de neuronnes

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ from random import *
 import arcade
 import pickle
 import os
+from sklearn.neural_network import MLPRegressor
 
 SPRITE_SIZE = 80
 
@@ -83,45 +84,82 @@ class Environment:
 class Agent:
     def __init__(self, environment):
         self.__environment = environment
-        self.__qtable = {}
+        #self.__qtable = {}
+
         self.__learning_rate = 1
         self.__discount_factor = 1
-        for s in environment.states:
-            self.__qtable[s] = {}
-            for a in ACTIONS:
-                self.__qtable[s][a] = random() * 10.0
+        #for s in environment.states:
+        #    self.__qtable[s] = {}
+        #    for a in ACTIONS:
+        #        self.__qtable[s][a] = random() * 10.0
+        self.__history = []
+        self.__exploration = 1.0
+
+        self.__mlp = MLPRegressor(hidden_layer_sizes=(10,),
+                                  activation='logistic',
+                                  solver='sgd',
+                                  max_iter=1,
+                                  warm_start=True,
+                                  learning_rate_init=self.__learning_rate)
+        self.__mlp.fit([[0, 0]], [[0] * len(ACTIONS)])  # initialisation du RN
         self.reset()
 
     def save(self, filename):
         with open(filename, 'wb') as file:
-            pickle.dump(self.__qtable, file)
+            pickle.dump(self.__mlp, file)
 
     def load(self, filename):
         with open(filename, 'rb') as file:
-            self.__qtable = pickle.load(file)            
+            self.__mlp = pickle.load(file)
 
     def reset(self):
         self.__state = self.__environment.start
         self.__score = 0
         self.__last_action = None
 
+    def update_history(self):
+        self.__history.append(self.__score)
+
+    @property
+    def history(self):
+        return self.__history
+
     def update(self, action, state, reward):
-        maxQ = max(self.__qtable[state].values())
-        self.__qtable[self.__state][action] += self.__learning_rate * \
-                                (reward + self.__discount_factor * \
-                                 maxQ - self.__qtable[self.__state][action])
+        maxQ = max(self.__mlp.predict([self.state_to_vector(state)])[0])
+        desired = reward + self.__discount_factor * maxQ
+
+        qvector = self.__mlp.predict([self.state_to_vector(self.__state)])[0]
+        i_action = ACTIONS.index(action)
+        qvector[i_action] = desired
+
+        self.__mlp.fit([self.state_to_vector(self.__state)], [qvector])
 
         self.__state = state
         self.__score += reward
         self.__last_action = action
 
+    def state_to_vector(self, state):
+        return [state[0] / self.__environment.width, \
+                state[1] / self.__environment.height]
+
     def best_action(self):
-        rewards = self.__qtable[self.__state]
-        best = None
-        for a in rewards:
-            if best is None or rewards[a] > rewards[best]:
-                best = a
+        if random() < self.__exploration:
+            best = choice(ACTIONS)  # une action au hasard
+            self.__exploration *= 0.99
+            # print(self.__exploration)
+        else:
+            qvector = self.__mlp.predict([self.state_to_vector(self.__state)])[0]
+            i_best = 0
+            for i in range(len(qvector)):
+                if qvector[i] > qvector[i_best]:
+                    i_best = i
+            best = ACTIONS[i_best]
+
         return best
+
+    @property
+    def exploration(self):
+        return self.__exploration
 
     def do(self, action):
         self.__environment.apply(self, action)
@@ -135,8 +173,8 @@ class Agent:
         return self.__score
 
     @property
-    def qtable(self):
-        return self.__qtable
+    def mlp(self):
+        return self.__mlp
 
     @property
     def environment(self):
